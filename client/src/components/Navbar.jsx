5014c5650ab71b8bc313067b8abe299ab7824447
JBWERE-coder <lutherwere@gmail.com>
2023-11-24 18:56:56 +0300

    icons/navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white flex justify-between items-center h-24  max-auto px-4 '>
+        <h1 className='w-full text 3xl font-bold text-[#00df9a] '>ReadsBySuki</h1>
+        <ul className='flex'>
+            <li className='p-4'>Blog</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
