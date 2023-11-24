1b73f5485559d9eacd0b89b6b1598a45d09ec451
JBWERE-coder <lutherwere@gmail.com>
2023-11-25 00:48:50 +0300

    hero section done

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,9 +7,17 @@ const Hero = () => {
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
       <p className='text-[#00df9a] font-bold p-2 text-3xl'>ReadsBySuki Blog</p>
       <h1 className='font-bold text-xl'>Grow & Learn </h1>
-      <div>
+      <div className='flex justify-center'>
         <p className='font-bold'>Read all topics from </p>
+        <Typed 
+        className='font-bold pl-2'
+        strings={['Love', 'Sex','Money', 'Relationships','Parenting']} 
+        typeSpeed={120} 
+        backSpeed={120}
+        loop
+        />
       </div>
+      <p className='italic  font-bold'>Enjoy every line you read...</p>
       </div>
     </div>
   )
